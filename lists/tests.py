12beb3d49e88263456bfebac05fa0ecb8fd3e2c6
Rafael Perez <rafael.perez@dcc.ufmg.br>
2020-09-13 23:41:15 -0300

    Implementando o Novo Projeto: Ajustando o Resto do Sistema

--- lists/tests.py
+++ lists/tests.py
@@ -47,8 +47,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        my_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=my_list)
+        Item.objects.create(text='itemey 2', list=my_list)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -66,8 +67,4 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
-
-def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/lists/the-only-list-in-the-world/')
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
@@ -7,3 +7,8 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    my_list = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=my_list)
+    return redirect('/lists/the-only-list-in-the-world/')
